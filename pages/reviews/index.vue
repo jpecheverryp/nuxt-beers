cdcd9d6e106f91587c742d9d3382f60f73842150
Juan P Echeverry <70663853+jpecheverryp@users.noreply.github.com>
2022-06-12 11:53:16 -0400

    styled search form in reviews page

--- pages/reviews/index.vue
+++ pages/reviews/index.vue
@@ -1,8 +1,9 @@
 <template>
   <div>
     <h2 class="text-2xl text-center">Reviews</h2>
-    <form class="text-center">
-      <input type="text" class="border border-gray-200 shadow rounded-3xl mx-auto py-1 px-3" placeholder="Search">
+    <form class="text-center relative w-60 mx-auto mt-4">
+      <input type="text" class="border border-gray-200 shadow rounded-3xl mx-auto py-1 pl-8" placeholder="Search">
+      <input type="button" class="absolute right-0 h-full rounded-r-3xl px-4" value="Search">
     </form>
 
     <review-card-list :reviews="reviews" />
